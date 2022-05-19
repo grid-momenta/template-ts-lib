c28687c2995df17d86d2d77108a31e11f72148d5
DHN Chandan <php.chandan@gmail.com>
2022-05-19 19:38:30 +0600

    chore: :art: Config and apply prettier
    prettier . --write (global prettier)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 require("@rushstack/eslint-config/patch/modern-module-resolution");
 
 module.exports = {
-	extends: [
-		"@rushstack/eslint-config/profile/node-trusted-tool",
-		"@rushstack/eslint-config/mixins/friendly-locals",
-	],
+	extends: ["@rushstack/eslint-config/profile/node-trusted-tool", "@rushstack/eslint-config/mixins/friendly-locals"],
 	parserOptions: { tsconfigRootDir: __dirname },
 	rules: {
 		"@typescript-eslint/naming-convention": [
